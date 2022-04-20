9916063d3b942d13ec4cabca5ba92c2dc2762577
rahmahkn <rahmah.ks.nurdini@gmail.com>
2022-04-20 23:38:48 +0700

    Fix cow

--- src/model/models/cat.ts
+++ src/model/models/cat.ts
@@ -46,22 +46,22 @@ export class Cat extends Model {
             this.updateTransform();
         });
 
-        const xCenter = 0;
+        const xCenter = -2;
         const yCenter = 0;
         const scale = 1;
         const cubeSize = 2;
-        const bodySize = [1.5 / scale, 1.5 / scale, 2 / scale];
-        const neckSize = [1 / scale, 0.1 / scale, 0.1 / scale];
-        const headSize = [1 / scale, 0.5 / scale, 1 / scale];
-        const earSize = [0.25 / scale, 0.1 / scale, 0.3 / scale];
-        const tailSize = [0.05 / scale, 0.2 / scale, 1.75 / scale];
+        const bodySize = [1 / scale, 1 / scale, 3 / scale];
+        const neckSize = [1 / scale, 0.4 / scale, 0.1 / scale];
+        const headSize = [1 / scale, 1 / scale, 1 / scale];
+        const earSize = [0.1 / scale, 0.2 / scale, 0.1 / scale];
+        const tailSize = [0.1 / scale, 0.2 / scale, 1.5 / scale];
         const legSize = [0.2 / scale, 0.8 / scale, 0.2 / scale];
 
-        const kScaleBody = [bodySize[0] / cubeSize, bodySize[1] / cubeSize, bodySize[2] / cubeSize]
-        const kScaleNeck = [neckSize[0] / cubeSize, neckSize[1] / cubeSize, neckSize[2] / cubeSize]
-        const kScaleHead = [headSize[0] / cubeSize, headSize[1] / cubeSize, headSize[2] / cubeSize]
-        const kScaleEar = [earSize[0] / cubeSize, earSize[1] / cubeSize, earSize[2] / cubeSize]
-        const kScaleTail = [tailSize[0] / cubeSize, tailSize[1] / cubeSize, tailSize[2] / cubeSize]
+        const kScaleBody = [bodySize[0] / cubeSize, bodySize[1] / cubeSize, bodySize[2] / cubeSize];
+        const kScaleNeck = [neckSize[0] / cubeSize, neckSize[1] / cubeSize, neckSize[2] / cubeSize];
+        const kScaleHead = [headSize[0] / cubeSize, headSize[1] / cubeSize, headSize[2] / cubeSize];
+        const kScaleEar = [earSize[0] / cubeSize, earSize[1] / cubeSize, earSize[2] / cubeSize];
+        const kScaleTail = [tailSize[0] / cubeSize, tailSize[1] / cubeSize, tailSize[2] / cubeSize];
         const kScaleLeg = [legSize[0] / cubeSize, legSize[1] / cubeSize, legSize[2] / cubeSize];
 
         const translateBody = [xCenter, 0, 0];
